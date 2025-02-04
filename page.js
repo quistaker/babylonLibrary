9a128a80ddc00d1d4468377955f74f0b94c23cf9
Iskander Mamedov <ahiskali1@gmail.com>
2025-02-04 21:30:58 +0300

    Generate lines with endline symbols

--- page.js
+++ page.js
@@ -1,16 +1,25 @@
+import "./helpers.js"
+
+const LINE_LENGTH = 80
+const LINE_COUNT = 40
+const ALPHABET = 'abcdefgihjklmnopqrstuvwxyz ,.'.split('')
 const workText = document.getElementsByClassName('worktext')[0]
 const randButton = document.getElementsByClassName('randBtn')[0]
-const alphabet = 'abcdefgihjklmnopqrstuvwxyz ,.'
-const alphabetArray = alphabet.split('')
 
+randButton.addEventListener('click', update_text)
 
+function update_text() {
+  workText.textContent = generate_text()
+}
 
-randButton.addEventListener('click', function(e) {
+function generate_text() {
+  return Array(LINE_COUNT).fill().map( () => generate_line() ).join("\n")
+}
 
-  workText.textContent = workText.textContent
-  .split('')
-    .map(
-      (el) => el = alphabetArray[Math.floor(Math.random()*alphabetArray.length)])
-    .join('')
-    console.log(workText.textContent.split(''))
-})
+function generate_line() {
+  return Array(LINE_LENGTH).fill().map( () => ALPHABET.sample() ).join('')
+}
+
+window.onload = function() {
+  update_text()
+}
